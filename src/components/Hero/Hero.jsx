1575de5e17347131a6431594b5817e84b573d5ad
aliraza-090 <alirazachakrani090@gmail..com>
2025-09-27 01:22:53 +0500

    Refactored Hero section layout and CSS for better alignment and design

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../assets/hand_icon.png'
+import arrow from '../assets/arrow.png'
+import hero_image from '../assets/hero_image.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-left">
+<h2>NEW ARRIVAL ONLY</h2>
+      </div>
+<div className="hero-hand-icon">
+    <p>new</p>
+    <img src={hand_icon} alt="" />
+</div>
+<p>collection</p>
+<p>for everyone</p>
+<div className="hero-latest-button">
+    <div>latest collection</div>
+    <img src={arrow} alt="" />
+</div>
+      <div className="hero-right">
+<img src={hero_image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
